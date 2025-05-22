b4376a5aff624f52d69e59da3f16c0e207aae5a9
Reyckband <Twandekock@gmail.com>
2025-05-22 14:40:48 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,7 +48,7 @@ const Home: React.FC = () => {
               ${animate ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
             style={{ transitionDelay: '1000ms' }}
           >
-            Nederrock
+            Onversneden Nederrock
           </p>
           <div 
             className={`relative transition-all duration-1000 transform
