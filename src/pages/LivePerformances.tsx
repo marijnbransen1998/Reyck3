5f3dad0010d1d8599226ddcf426e9c94c4cf9a47
Reyckband <Twandekock@gmail.com>
2025-06-13 11:30:27 +0200

    Update LivePerformances.tsx

--- src/pages/LivePerformances.tsx
+++ src/pages/LivePerformances.tsx
@@ -32,9 +32,10 @@ const LivePerformances: React.FC = () => {
             {/* Optredens Grid */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 relative">
               {[
-                { title: "Vort met de Geit", date: "30 augustus", location: "Lexmond" },
-                { title: "SPOEL Festival", date: "31 augustus", location: "Culemborg" },
-                { title: "Bergen Live", date: "5 september", location: "Bergen" },
+                  { title: "Enno Lloyd EP Release", date: "10 juli", location: "Zaandam, Het Motorblok" },
+                   { title: "Vort met de Geit", date: "30 augustus", location: "Lexmond" },
+                 { title: "SPOEL Festival", date: "31 augustus", location: "Culemborg" },
+                 { title: "Bergen Live", date: "5 september", location: "Bergen" },
                 { title: "TO BE ANNOUNCED", date: "", location: "", link: "", hasTickets: false }
               ].map((gig, index) => (
                 <div 
