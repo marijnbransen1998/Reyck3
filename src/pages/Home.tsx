43341aa2bfcbf71c7bd115dce8933dd0b8ed5669
Reyckband <Twandekock@gmail.com>
2025-06-13 11:29:11 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -82,6 +82,7 @@ const Home: React.FC = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               {[
+  { title: "Enno Lloyd EP Release", date: "10 juli", location: "Zaandam, Het Motorblok" },
   { title: "Vort met de Geit", date: "30 augustus", location: "Lexmond" },
   { title: "SPOEL Festival", date: "31 augustus", location: "Culemborg" },
  { title: "Bergen Live", date: "5 september", location: "Bergen" },
