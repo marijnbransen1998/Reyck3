726c92da1d9ff8bddb92ad22d8eea96dc85d807a
Reyckband <Twandekock@gmail.com>
2025-07-17 15:46:28 +0200

    Update LivePerformances.tsx

--- src/pages/LivePerformances.tsx
+++ src/pages/LivePerformances.tsx
@@ -32,8 +32,7 @@ const LivePerformances: React.FC = () => {
             {/* Optredens Grid */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 relative">
               {[
-  { title: "Enno Lloyd EP Release", date: "10 juli, 20:00", location: "Zaandam, Het Motorblok" },
-  { title: "Vort met de Geit", date: "30 augustus", location: "Lexmond, Kom Lekdijk 38" },
+  { title: "Vort met de Geit", date: "30 augustus, 18:30", location: "Lexmond, Kom Lekdijk 38" },
   { title: "SPOEL Festival", date: "31 augustus, 15:15", location: "Culemborg, Werk aan het Spoel" },
  { title: "Bergen Live", date: "5 september", location: "Bergen" },
                 { title: "TO BE ANNOUNCED", date: "", location: "", link: "", hasTickets: false }
