65c0a807ee4c22842cf3ca32705102eb2383361a
Marijn Bransen <123900953+marijnbransen1998@users.noreply.github.com>
2025-11-19 18:51:52 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -224,7 +224,7 @@ const Home: React.FC = () => {
           {/* Instagram Feed */}
           <section ref={instagramRef} className="relative z-10">
             <h2 className="text-4xl md:text-4xl font-bold mb-8 text-center text-white font-optien tracking-wider">
-              FOLLOW US ON INSTAGRAM
+              FOLLOW REYCK ON INSTAGRAM
             </h2>
             <div className="p-6 block instagram-widget-container">
               <div className="elfsight-app-971ba9a9-b75b-4ca1-95d1-b752ac7d0b22" data-elfsight-app-lazy></div>
