3ccf18c8466ac2d82d5bc4729468b3c511dc8bd6
Reyckband <Twandekock@gmail.com>
2026-01-05 09:24:40 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -114,8 +114,9 @@ const Home: React.FC = () => {
             </h2>
             <div className="max-w-4xl mx-auto space-y-8">
               {[
-                { title: "'T OUDE POTHUYS", date: "21", month: "NOV.", location: "UTRECHT", time: "23:00" },
-                { title: "TOEKOMSTMUZIEK", date: "22", month: "NOV.", location: "AMSTERDAM", time: "20:00" },
+                { title: "PENGUIN RADIO SHOWCASES", date: "16", month: "JAN.", location: "GRONINGEN", time: "02:15" },
+                { title: "SKEK", date: "12", month: "FEB.", location: "AMSTERDAM", time: "20:00" },
+                { title: "CLUB 3VOOR12 DBS", date: "26", month: "MRT.", location: "UTRECHT", time: "20:30" },
               ].map((gig, index) => (
                 <div
                   key={index}
