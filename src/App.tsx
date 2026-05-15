18d765f999eb4477140cc91a65a04a5eeb0e085a
Reyckband <Twandekock@gmail.com>
2026-05-15 21:33:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import Biography from './pages/Biography';
-import MusicPage from './pages/Music';
-import LivePerformances from './pages/LivePerformances';
-import Contact from './pages/Contact';
 import ContactButtons from './components/WhatsAppButton';
-import ScrollToTop from './components/ScrollToTop'; // ✅ import
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Header />
-        <ScrollToTop /> {/* ✅ scroll naar boven bij routewissel */}
+
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/biografie" element={<Biography />} />
-            <Route path="/muziek" element={<MusicPage />} />
-            <Route path="/optredens" element={<LivePerformances />} />
-            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
+
         <Footer />
         <ContactButtons />
+
       </div>
     </Router>
   );
